f5583d6dce65e4e9e0852a52e01ea1850b7f6514
Lucas Reis M. Dias <thereis@live.com>
2018-05-09 19:24:58 -0300

    Updates to client

--- client.js
+++ client.js
@@ -1,36 +1,36 @@
 import { readFileSync } from "fs";
 import { chunker } from "./utils";
 
+/**
+ * Load the file synchronous
+ */
 const loadFile = async () => {
   console.log("Loading file...");
 
   let content = await readFileSync("data.in", "utf8");
 
-  console.log("Loaded! Returning statement...");
-  
   return content.toString().split("\n");
 };
 
+/**
+ * Main application function
+ */
 const init = async () => {
-  console.log("Loading v");
   let numbers = await loadFile();
+
+  console.log("File loaded!");
+
+  /**
+   * Numbers configuration
+   */
   let numbersLength = numbers.length;
 
   const maxChunks = 3;
   const chunkDivisor = Math.ceil(numbersLength / maxChunks);
   const chunks = chunker(numbers, chunkDivisor);
-
-  // Initializes chunks array
-  /*for (let i = 1; i <= maxChunks; i++) {
-    let test = numbers.splice(chunkAux, chunkDivisor * i);
-
-    console.log(test);
-
-    chunkAux = test.length - 1;
-  }*/
-
-  //console.log(chunksMap);
 };
 
-// Initialize main application
+/**
+ * Initialize the main application
+ */
 init();
